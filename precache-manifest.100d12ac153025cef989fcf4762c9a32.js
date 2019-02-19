7625ba6bc4d91967c81884f6644886d23c5e3060
Gustavo Evangelista <gus2893@gmail.com>
2019-02-19 14:44:35 -0500

    Updates

---
+++ precache-manifest.100d12ac153025cef989fcf4762c9a32.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "14c3a7b1f54ac927f789",
+    "url": "/Pokedex/static/js/main.14c3a7b1.chunk.js"
+  },
+  {
+    "revision": "27f1668b3bbe8710c9a9",
+    "url": "/Pokedex/static/js/runtime~main.27f1668b.js"
+  },
+  {
+    "revision": "f141a1af1b2ef87c8c23",
+    "url": "/Pokedex/static/js/2.f141a1af.chunk.js"
+  },
+  {
+    "revision": "9fb1eb4417fa7c0e9ce59d0b0637d79a",
+    "url": "/Pokedex/index.html"
+  }
+];
