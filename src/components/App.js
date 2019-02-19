1c5badb5ed46a97ce4d5051592e49b03d70ee063
Gustavo Evangelista <gus2893@gmail.com>
2019-02-19 14:18:03 -0500

    finished application

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import {fetchPokemon} from '../actions';
 import {Button, Container, Segment, Grid, Divider, Icon} from 'semantic-ui-react';
 
 class App extends Component {
-    state = {next: false, prev:true}
+    state = {next: false, prev:true, details: false, picture: true}
     
     componentDidMount = () =>{
         this.props.fetchPokemon(1);
@@ -16,47 +16,84 @@ class App extends Component {
     }
     render(){
         const {pokemon} = this.props;
-
         return (
-            <Container Align='center' >
-             <Segment Align='center' compact inverted color='red' style={{borderRadius: '50px'}}>
-                <Segment Align='center' compact padded='very' inverted color='black'  style={{borderRadius: '18px'}}>
-                    <PokemonInfo pokemon = {pokemon.pokemonData}/>      
+            <Container align='center' >
+                <Segment align='center' compact inverted color='red' style={{borderRadius: '50px'}}>
+                <Segment align='center'  padded='very' inverted color='black'  style={{borderRadius: '18px'}}>
+                <Segment size='large' padded style={{ backgroundColor:'lightgrey'}}>
+                    <PokemonInfo pokemon = {pokemon.pokemonData} details ={this.state.details} picture ={this.state.picture} /> 
+                </Segment>
+                              
                 </Segment>
                 <Divider/>
-                <Grid columns={2}  >
-                    <Grid.Row>
-                        <Grid.Column width={1} verticalAlign='middle' centered >
-                            <Icon 
-                                bordered inverted color='black'  
-                                name='caret left' 
-                                disabled={this.state.prev} 
-                                onClick={()=> this.navigate(pokemon.pokemonId-1)}
-                            />
-                        </Grid.Column>
-                        <Grid.Column width={1} verticalAlign='middle'centered >
-                            <Icon bordered inverted color='black'  name='caret up'></Icon>
-                            <Icon bordered inverted color='black'  name='circle'></Icon>
-                            <Icon bordered inverted color='black'  name='caret down'></Icon>
-                        </Grid.Column>
-                        <Grid.Column width={1} verticalAlign='middle' centered >
-                            <Icon bordered inverted color='black'  name='caret right' disabled={this.state.next} onClick={()=> this.navigate(pokemon.pokemonId+1)}></Icon>
+                <Divider/>
+                <Grid columns={3} >
+                    <Grid.Row >
+                        <Grid.Column  width={1}>    
                         </Grid.Column>
 
-                        <Grid.Column width={11} floated='right'>
-                            <Button content='B' circular color='black' compact></Button>
-                            <Button content='A' circular color='black' compact></Button>
+                        <Grid.Column  width={13}>
+                            <Grid.Column  verticalAlign='middle' align='left' style={{paddingLeft: '35px', paddingBottom: '4px'}}>
+                                <Icon bordered inverted color='black'  name='caret up' disabled={this.state.next} onClick={()=> this.navigate(pokemon.pokemonId+1)}></Icon>
+                            </Grid.Column>
+                            
+                            
+                            <Icon bordered inverted color='black'  name='caret left' disabled={this.state.picture} onClick ={()=> this.setState({picture : true})}/>
+                            <Icon bordered inverted color='black'  name='circle' style={{paddingLeft: '35px'}}></Icon>
+                            <Icon bordered inverted color='black'  name='caret right' disabled={!this.state.picture} onClick ={()=> this.setState({picture : false})}></Icon>
+                            
+                            <Button 
+                                content='A' 
+                                circular 
+                                color='black' 
+                                compact 
+                                floated='right'
+                                onClick ={()=> this.setState({details : true})}>
+                            </Button>
+                            <Button 
+                                content='B' 
+                                circular 
+                                color='black' 
+                                compact 
+                                floated='right'
+                                onClick ={()=> this.setState({details : false})}>
+                            </Button>
+
+                            
+
+                            <Grid.Column width={1} verticalAlign='middle' style={{paddingRight: '88px', paddingTop: '4px'}}>
+                                <Icon 
+                                    bordered inverted color='black'  
+                                    name='caret down'                                     
+                                    disabled={this.state.prev} 
+                                    onClick={()=> this.navigate(pokemon.pokemonId-1)}>
+                                </Icon>
+                            </Grid.Column>
+                            <Divider/>
+                            <Grid.Column width={9}>
+                                <Button size='mini' content='Select' circular color='black' compact>
+                                </Button>
+                                <Button size='mini' content='Start' circular color='black' compact>
+                                </Button>
+                            </Grid.Column>
+                            
+                                
+                            
+                        
+                   
+
+                        </Grid.Column>
+
+                        <Grid.Column width={1}>
                         </Grid.Column>
 
                     </Grid.Row>
 
-                    <Grid.Row>
+                    <Grid.Row  >
 
                     </Grid.Row>
                 </Grid>
-             
-             </Segment>
-
+                </Segment>
             </Container>     
         );
     }
--- src/components/PokemonInfo.js
+++ src/components/PokemonInfo.js
@@ -1,18 +1,47 @@
 import React from 'react';
-import {Image, Container, Segment} from 'semantic-ui-react';
+import {Image} from 'semantic-ui-react';
 
-const PokemonInfo = ({pokemon}) =>{
+const PokemonInfo = ({pokemon , details, picture}) =>{
+    
     if(!pokemon){
         return null;
     }
+    const pokemonPicture = (picture) ? pokemon.sprites.front_default : pokemon.sprites.back_default;
 
+    let type1, type2 = '';
+    if(pokemon.types[1]){
+        type1 = pokemon.types[0].type.name;
+        type2 = pokemon.types[1].type.name;
+    }else {
+        type1 = pokemon.types[0].type.name;
+    }
+    
+    if(details){
+        return (
+            <div>
+                {pokemon.name} #{pokemon.id}<br/>
+                <br/>
+                Weight: {(pokemon.weight/4.536).toFixed(0)}lbs<br/>
+                Height: {(pokemon.height*10).toFixed(0)}cm<br/>
+                Types: <br/>
+                {type1}<br/>
+                {type2}
 
-    return (
-    <Segment compact padded style={{ backgroundColor:'lightgrey'}}>
-        <Image src={pokemon.sprites.front_default}/>
-        {pokemon.name}
-    </Segment>
-    );  
+            </div>
+            ); 
+
+    }else{
+        return (
+            <div>
+                <div style={{float:'right'}}>#{pokemon.id}</div><br/>
+                <Image src={pokemonPicture}/>
+                {pokemon.name}<br/>
+            </div>
+            ); 
+    }
+    
+
+ 
 }
 
 export default PokemonInfo;
